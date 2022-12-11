72ce3d4fb9dc35da33dfa8d579d968b018f39943
Sajeevan <sajeevanjspy@gmail.com>
2022-12-11 13:56:58 +0530

    mod-test: primitive values

--- packages/shared/__tests__/toDisplayString.spec.ts
+++ packages/shared/__tests__/toDisplayString.spec.ts
@@ -5,4 +5,11 @@ describe('toDisplayString', () => {
     expect(toDisplayString(null)).toBe('')
     expect(toDisplayString(undefined)).toBe('')
   })
+
+  test('primitive values', () => {
+    expect(toDisplayString(1)).toBe('1')
+    expect(toDisplayString(true)).toBe('true')
+    expect(toDisplayString(false)).toBe('false')
+    expect(toDisplayString('hello')).toBe('hello')
+  })
 })
