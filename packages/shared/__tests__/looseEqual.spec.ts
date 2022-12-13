ff467f29a5d4b4b3d5db0e58c67647bb7bcefefc
Sajeevan <sajeevanjspy@gmail.com>
2022-12-13 09:05:49 +0530

    mod-test: shared/looseEqual | compares strings correctly

--- packages/shared/__tests__/looseEqual.spec.ts
+++ packages/shared/__tests__/looseEqual.spec.ts
@@ -8,4 +8,15 @@ describe('utils/looseEqual', () => {
     expect(looseEqual(true, 1)).toBe(false)
     expect(looseEqual(false, 0)).toBe(false)
   })
+
+  test('compares strings correctly', () => {
+    const text = 'Lorem ipsum'
+    const number = 1
+    const bool = true
+
+    expect(looseEqual(text, text)).toBe(true)
+    expect(looseEqual(text, text.slice(0, -1))).toBe(false)
+    expect(looseEqual(String(number), number)).toBe(true)
+    expect(looseEqual(String(bool), bool)).toBe(true)
+  })
 })
