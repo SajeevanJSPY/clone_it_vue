5be9ba99a80bf283738bd3852e45a423188dc634
Sajeevan <sajeevanjspy@gmail.com>
2022-12-13 17:00:30 +0530

    mod-create: reactivity/effect | created essent types

---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,42 @@
+import { TrackOpTypes, TriggerOpTypes } from './operations'
+import { EMPTY_OBJ, isArray, isIntegerKey, isMap } from '@vue/shared'
+
+// The main WeakMap that stores {target -> key -> dep} connections.
+// Conceptually, it's easier to think of a dependency as a Dep class
+// which maintains a Set of subscribers, but we simply store them as
+// raw Sets to reduce memory overhead.
+type Dep = Set<ReactiveEffect>
+type KeyToDepMap = Map<any, any>
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
+export interface ReactiveEffect<T = any> {
+  (): T
+  _isEffect: true
+  id: number
+  active: boolean
+  raw: () => T
+  deps: Array<Dep>
+  options: ReactiveEffectOptions
+}
+
+export interface ReactiveEffectOptions {
+  lazy?: boolean
+  schedular?: (job: ReactiveEffect) => void
+  onTrack?: (event: DebuggerEvent) => void
+  onTrigger?: (event: DebuggerEvent) => void
+  onStop?: () => void
+  allowRecurse?: boolean
+}
+
+export type DebuggerEvent = {
+  effect: ReactiveEffect
+  target: Object
+  type: TrackOpTypes | TriggerOpTypes
+  key: any
+} & DebuggerEventExtraInfo
+
+export interface DebuggerEventExtraInfo {
+  newValue?: any
+  oldValue?: any
+  oldTarget?: Map<any, any> | Set<any>
+}
