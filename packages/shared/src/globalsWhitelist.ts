fc4b2c0b0e6004b348b0bbe0de71baa15bf09480
Sajeevan <sajeevanjspy@gmail.com>
2022-12-19 10:02:06 +0530

    mod-update: globalsWhitelist

--- packages/shared/src/globalsWhitelist.ts
+++ packages/shared/src/globalsWhitelist.ts
@@ -1,8 +1,8 @@
 import { makeMap } from './makeMap'
 
-const GlOBALS_WHITE_LISTED =
+const GLOBALS_WHITE_LISTED =
   'Infinity,undefined,NaN,isFinite,isNaN,parseFloat,parseInt,decodeURI,' +
   'decodeURIComponent,encodeURI,encodeURIComponent,Math,Number,Date,Array,' +
-  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl'
+  'Object,Boolean,String,RegExp,Map,Set,JSON,Intl,BigInt'
 
-export const isGloballyWhitelisted = /*#__PURE__*/ makeMap(GlOBALS_WHITE_LISTED)
+export const isGloballyWhitelisted = /*#__PURE__*/ makeMap(GLOBALS_WHITE_LISTED)
