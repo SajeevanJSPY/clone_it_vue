69325cd0b627268281ed064aae7dba7d736bab67
Sajeevan <sajeevanjspy@gmail.com>
2022-12-19 10:03:45 +0530

    mod-update: shared/index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,27 +1,28 @@
 import { makeMap } from './makeMap'
 
-/*
- * List of @babel/parser plugins that are used for template expression
- * transforms and SFC script tranforms. By default we enable proposals slated
- * for ES2020. This will need to be updated as the spec moves forward.
- * Full list at https://babeljs.io/docs/en/next/babel-parser#plugins
- */
-
-export const babelParserDefaultPlugins = [
-  'bigInt',
-  'optionalChaining',
-  'nullishCoalescingOperator',
-] as const
+export { makeMap }
+export * from './patchFlags'
+export * from './shapeFlags'
+export * from './slotFlags'
+export * from './globalsWhitelist'
+export * from './codeframe'
+export * from './normalizeProp'
+export * from './domTagConfig'
+export * from './domAttrConfig'
+export * from './escapeHtml'
+export * from './looseEqual'
+export * from './toDisplayString'
+export * from './typeUtils'
 
 export const EMPTY_OBJ: { readonly [key: string]: any } = __DEV__
   ? Object.freeze({})
   : {}
-export const EMPTY_ARR: [] = []
+export const EMPTY_ARR = __DEV__ ? Object.freeze([]) : []
 
 export const NOOP = () => {}
 
-/*
- * Always return false
+/**
+ * Always return false.
  */
 export const NO = () => false
 
@@ -51,7 +52,10 @@ export const isMap = (val: unknown): val is Map<any, any> =>
 export const isSet = (val: unknown): val is Set<any> =>
   toTypeString(val) === '[object Set]'
 
-export const isDate = (val: unknown): val is Date => val instanceof Date
+export const isDate = (val: unknown): val is Date =>
+  toTypeString(val) === '[object Date]'
+export const isRegExp = (val: unknown): val is RegExp =>
+  toTypeString(val) === '[object RegExp]'
 export const isFunction = (val: unknown): val is Function =>
   typeof val === 'function'
 export const isString = (val: unknown): val is string => typeof val === 'string'
@@ -68,6 +72,7 @@ export const toTypeString = (value: unknown): string =>
   objectToString.call(value)
 
 export const toRawType = (value: unknown): string => {
+  // extract "RawType" from strings like "[object RawType]"
   return toTypeString(value).slice(8, -1)
 }
 
@@ -81,10 +86,15 @@ export const isIntegerKey = (key: unknown) =>
   '' + parseInt(key, 10) === key
 
 export const isReservedProp = /*#__PURE__*/ makeMap(
-  'key,ref,' +
+  // the leading comma is intentional so empty string "" is also included
+  ',key,ref,ref_for,ref_key,' +
     'onVnodeBeforeMount,onVnodeMounted,' +
-    'onVnodeBeforeUpdate,onVnodeUpdated' +
+    'onVnodeBeforeUpdate,onVnodeUpdated,' +
     'onVnodeBeforeUnmount,onVnodeUnmounted'
+)
+
+export const isBuiltInDirective = /*#__PURE__*/ makeMap(
+  'bind,cloak,else-if,else,for,html,if,model,on,once,pre,show,slot,text,memo'
 )
 
 const cacheStringFunction = <T extends (str: string) => string>(fn: T): T => {
@@ -92,11 +102,11 @@ const cacheStringFunction = <T extends (str: string) => string>(fn: T): T => {
   return ((str: string) => {
     const hit = cache[str]
     return hit || (cache[str] = fn(str))
-  }) as any
+  }) as T
 }
 
 const camelizeRE = /-(\w)/g
-/*
+/**
  * @private
  */
 export const camelize = cacheStringFunction((str: string): string => {
@@ -104,23 +114,30 @@ export const camelize = cacheStringFunction((str: string): string => {
 })
 
 const hyphenateRE = /\B([A-Z])/g
-/*
+/**
  * @private
  */
-export const hyphenate = cacheStringFunction((str: string): string => {
-  return str.replace(hyphenateRE, '-$1').toLowerCase()
-})
+export const hyphenate = cacheStringFunction((str: string) =>
+  str.replace(hyphenateRE, '-$1').toLowerCase()
+)
 
-/*
+/**
  * @private
  */
-export const capitalize = cacheStringFunction((str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-})
+export const capitalize = cacheStringFunction(
+  (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+)
 
-// compare whether a value has changed, accounting for NaN
+/**
+ * @private
+ */
+export const toHandlerKey = cacheStringFunction((str: string) =>
+  str ? `on${capitalize(str)}` : ``
+)
+
+// compare whether a value has changed, accounting for NaN.
 export const hasChanged = (value: any, oldValue: any): boolean =>
-  value !== oldValue && (value === value || oldValue === oldValue)
+  !Object.is(value, oldValue)
 
 export const invokeArrayFns = (fns: Function[], arg?: any) => {
   for (let i = 0; i < fns.length; i++) {
@@ -136,16 +153,29 @@ export const def = (obj: object, key: string | symbol, value: any) => {
   })
 }
 
-export const toNumber = (val: any): any => {
+/**
+ * "123-foo" will be parsed to 123
+ * This is used for the .number modifier in v-model
+ */
+export const looseToNumber = (val: any): any => {
   const n = parseFloat(val)
   return isNaN(n) ? val : n
 }
 
-let __globalThis: any
+/**
+ * Only conerces number-like strings
+ * "123-foo" will be returned as-is
+ */
+export const toNumber = (val: any): any => {
+  const n = isString(val) ? Number(val) : NaN
+  return isNaN(n) ? val : n
+}
+
+let _globalThis: any
 export const getGlobalThis = (): any => {
   return (
-    __globalThis ||
-    (__globalThis =
+    _globalThis ||
+    (_globalThis =
       typeof globalThis !== 'undefined'
         ? globalThis
         : typeof self !== 'undefined'
@@ -156,4 +186,12 @@ export const getGlobalThis = (): any => {
         ? global
         : {})
   )
+}
+
+const identRE = /^[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*$/
+
+export function genPropsAccessExp(name: string) {
+  return identRE.test(name)
+    ? `__props.${name}`
+    : `__props[${JSON.stringify(name)}]`
 }
