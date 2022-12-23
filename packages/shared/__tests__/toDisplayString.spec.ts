f5dc6775d2a532627477655a5d5061f18706a83a
Sajeevan <sajeevanjspy@gmail.com>
2022-12-23 12:33:27 +0530

    mod-test-update: shared/toDisplayString

--- packages/shared/__tests__/toDisplayString.spec.ts
+++ packages/shared/__tests__/toDisplayString.spec.ts
@@ -1,8 +1,4 @@
-/**
- * @jest-environment jsdom
- */
-
-import { toDisplayString } from '../src/toDisplayString'
+import { toDisplayString } from '../src'
 
 describe('toDisplayString', () => {
   test('nullish values', () => {
@@ -22,15 +18,142 @@ describe('toDisplayString', () => {
     expect(toDisplayString(obj)).toBe(JSON.stringify(obj, null, 2))
     const arr = [obj]
     expect(toDisplayString(arr)).toBe(JSON.stringify(arr, null, 2))
+
+    const objWithToStringOverride = {
+      foo: 555,
+      toString() {
+        return 'override'
+      }
+    }
+    expect(toDisplayString(objWithToStringOverride)).toBe('override')
+
+    const objWithNonInvokableToString = {
+      foo: 555,
+      toString: null
+    }
+    expect(toDisplayString(objWithNonInvokableToString)).toBe(
+      `{
+  "foo": 555,
+  "toString": null
+}`
+    )
+
+    // object created from null does not have .toString in its prototype
+    const nullObjectWithoutToString = Object.create(null)
+    nullObjectWithoutToString.bar = 1
+    expect(toDisplayString(nullObjectWithoutToString)).toBe(
+      `{
+  "bar": 1
+}`
+    )
+
+    // array toString override is ignored
+    const arrWithToStringOverride = [1, 2, 3]
+    arrWithToStringOverride.toString = () =>
+      'override for array is not supported'
+    expect(toDisplayString(arrWithToStringOverride)).toBe(
+      `[
+  1,
+  2,
+  3
+]`
+    )
+  })
+
+  test('objects with custom toString', () => {
+    class TestClass {
+      toString() {
+        return 'foo'
+      }
+    }
+    const instance = new TestClass()
+    expect(toDisplayString(instance)).toBe('foo')
+    const obj = { toString: () => 'bar' }
+    expect(toDisplayString(obj)).toBe('bar')
   })
 
   test('native objects', () => {
     const div = document.createElement('div')
-    expect(toDisplayString(div)).toBe(`"[object HTMLDivElement]"`)
-    // expect(toDisplayString({ div })).toMatchInlineSnapshot(`
-    //     {
-    //     "div": "[object HTMLDivElement]"
-    //     }
-    // `)
+    expect(toDisplayString(div)).toBe('[object HTMLDivElement]')
+    expect(toDisplayString({ div })).toMatchInlineSnapshot(`
+      "{
+        "div": "[object HTMLDivElement]"
+      }"
+    `)
+  })
+
+  test('Map and Set', () => {
+    const m = new Map<any, any>([
+      [1, 'foo'],
+      [{ baz: 1 }, { foo: 'bar', qux: 2 }]
+    ])
+    const s = new Set<any>([1, { foo: 'bar' }, m])
+
+    expect(toDisplayString(m)).toMatchInlineSnapshot(`
+      "{
+        "Map(2)": {
+          "1 =>": "foo",
+          "[object Object] =>": {
+            "foo": "bar",
+            "qux": 2
+          }
+        }
+      }"
+    `)
+    expect(toDisplayString(s)).toMatchInlineSnapshot(`
+      "{
+        "Set(3)": [
+          1,
+          {
+            "foo": "bar"
+          },
+          {
+            "Map(2)": {
+              "1 =>": "foo",
+              "[object Object] =>": {
+                "foo": "bar",
+                "qux": 2
+              }
+            }
+          }
+        ]
+      }"
+    `)
+
+    expect(
+      toDisplayString({
+        m,
+        s
+      })
+    ).toMatchInlineSnapshot(`
+      "{
+        "m": {
+          "Map(2)": {
+            "1 =>": "foo",
+            "[object Object] =>": {
+              "foo": "bar",
+              "qux": 2
+            }
+          }
+        },
+        "s": {
+          "Set(3)": [
+            1,
+            {
+              "foo": "bar"
+            },
+            {
+              "Map(2)": {
+                "1 =>": "foo",
+                "[object Object] =>": {
+                  "foo": "bar",
+                  "qux": 2
+                }
+              }
+            }
+          ]
+        }
+      }"
+    `)
   })
 })
