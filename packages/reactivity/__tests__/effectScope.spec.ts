ad61e07d78b49c597cd20c9fcf9a05c955a2e71f
Sajeevan <sajeevanjspy@gmail.com>
2023-01-06 18:15:09 +0530

    mod-test: reactivity/effectScope | nested scope can be escaped

--- packages/reactivity/__tests__/effectScope.spec.ts
+++ packages/reactivity/__tests__/effectScope.spec.ts
@@ -92,4 +92,33 @@ describe('reactivity/effect/scope', () => {
     expect(dummy).toBe(7)
     expect(doubled).toBe(14)
   })
+
+  it('nested scope can be escaped', () => {
+    let dummy, doubled
+    const counter = reactive({ num: 0 })
+
+    const scope = new EffectScope()
+    scope.run(() => {
+      effect(() => (dummy = counter.num))
+      // nested scope
+      new EffectScope(true).run(() => {
+        effect(() => (doubled = counter.num * 2))
+      })
+    })
+
+    expect(scope.effects.length).toBe(1)
+
+    expect(dummy).toBe(0)
+    counter.num = 7
+    expect(dummy).toBe(7)
+    expect(doubled).toBe(14)
+
+    scope.stop()
+
+    counter.num = 6
+    expect(dummy).toBe(7)
+
+    // nested scope should not be stopped
+    expect(doubled).toBe(12)
+  })
 })
